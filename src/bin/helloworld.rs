107c014f49498f6834a5c19d0e39222f4bfe0c68
Mora Unie Youer <mora_unie_youer@riseup.net>
2023-07-25 05:56:11 +0000

    Solution for example problems

---
+++ src/bin/helloworld.rs
@@ -0,0 +1,14 @@
+/**
+ * Hello, World!
+ *
+ * Welcome to the DMOJ!
+ *
+ * In this task, you must print out the message Hello, World! — the judge is very strict, so you must output it with the same
+ * capitalization and punctuation.
+ *
+ * Some example solutions in a couple of languages are shown below. After you've gotten the hang of submitting, try out a
+ * harder problem like A Plus B.
+ */
+fn main() {
+    println!("Hello, World!");
+}
---
+++ src/bin/scroll.rs
@@ -0,0 +1,22 @@
+/**
+ * Scroll
+ *
+ * While wandering through a forest, FatalEagle comes across a «Scroll». He's not too sure about the meaning of the contents
+ * of the «Scroll», just like he has his doubts about the content of this problem.
+ *
+ * Input Specification
+ * A single integer, N ( 0 ≤ N ≤ 100 ) . Just kidding, no input.
+ *
+ * Output Specification
+ * Who knows? Does it matter to you that much?
+ *
+ * Sample Input
+ * It might exist, but it probably doesn't.
+ *
+ * Sample Output
+ * Can sample output exist without sample input? If a tree falls in a forest and no one is around to hear it, does it make a
+ * sound? Is the previous question even relevant?
+ */
+fn main() {
+    println!("https://youtu.be/dQw4w9WgXcQ");
+}
